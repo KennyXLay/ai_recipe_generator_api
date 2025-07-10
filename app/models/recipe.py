c1027ece4bff3ac191861c873a76c7a65f48e469
KennyXLay <62919286+KennyXLay@users.noreply.github.com>
2025-07-09 19:45:33 -0700

    Added recipe model and pydantic schemes

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from app.db.session import Base
+from datetime import datetime
 
 class Recipe(Base):
     __tablename__ = "recipes"
@@ -9,6 +10,7 @@ class Recipe(Base):
     title = Column(String, nullable=False)
     ingredients = Column(String)
     instructions = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
-    owner = relationship("User", back_populates="recipes")
+    # owner = relationship("User", back_populates="recipes")
